46157f1c1463bad71ec75b71ae1a91cec66dbabb
Branimir Klarić <branimir.klaric.bk@gmail.com>
2018-03-25 23:32:01 +0200

    Add emitter phantom type to Event.

--- src/Node/Events.purs
+++ src/Node/Events.purs
@@ -1,11 +1,3 @@
 module Node.Events where
 
-import Prelude
-
-import Control.Monad.Effect.Exception (Error)
-import Control.Monad.Effect (Effect)
-
-newtype Event listener = Event String
-
-error :: Event (Error -> Effect Unit)
-error = Event "error"
+newtype Event emitter listener = Event String
